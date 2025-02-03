b9cb5091ac04612cdc92c825710a902fa23b31ce
bidha001 <147380207+bidha001@users.noreply.github.com>
2025-02-03 20:03:38 +0100

    Add files via upload

---
+++ Web-App.Project/src/main/java/Course.java
@@ -0,0 +1,10 @@
+public class Course {
+    private int id;
+    private String title;
+
+    public Course(int id, String tittle){
+        this.id = id;
+        this.title = title;
+        
+    }
+}
