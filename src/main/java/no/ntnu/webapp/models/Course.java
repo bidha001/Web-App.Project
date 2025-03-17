7a7b625e28b2aae6ccda7793bb894ae7a16e7da2
biritdhungana <binitd@stud.ntnu.no>
2025-03-17 13:56:08 +0100

    Add responsive design and enhance Course model with additional attributes

--- src/main/java/no/ntnu/webapp/models/Course.java
+++ src/main/java/no/ntnu/webapp/models/Course.java
@@ -1,5 +1,15 @@
 package no.ntnu.webapp.models;
 
+import java.util.Date;
+
 public class Course {
-  
+  private int id;
+  private String name;
+  private String description;
+  private Date startDate;
+  private Date endDate;
+  private double credits;
+  private double hours;
+  private Integer imageId;
+
 }
