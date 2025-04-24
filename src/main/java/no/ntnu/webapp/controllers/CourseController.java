141c1f0d668cd5064eb26a860a1bd0c792338484
biritdhungana <binitd@stud.ntnu.no>
2025-04-24 18:29:55 +0200

    Refactor CourseController and HTML files for Thymeleaf integration; implement navbar and footer as fragments, update links for improved routing and resource management

--- src/main/java/no/ntnu/webapp/controllers/CourseController.java
+++ src/main/java/no/ntnu/webapp/controllers/CourseController.java
@@ -1,5 +1,9 @@
 package no.ntnu.webapp.controllers;
 
+@Controller
 public class CourseController {
-  
+  @GetMapping("/home")
+    public String home() {
+        return "home"; // no .html extension needed
+    }
 }
