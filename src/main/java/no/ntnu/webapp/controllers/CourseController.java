3295ff9510016c7464bb7bc30c378770d55a661a
biritdhungana <binitd@stud.ntnu.no>
2025-04-24 18:44:45 +0200

    Rename CourseController to PageController for improved clarity; update package structure accordingly

--- src/main/java/no/ntnu/webapp/controllers/CourseController.java
+++
@@ -1,9 +0,0 @@
-package no.ntnu.webapp.controllers;
-
-@Controller
-public class CourseController {
-  @GetMapping("/home")
-    public String home() {
-        return "home"; // no .html extension needed
-    }
-}
